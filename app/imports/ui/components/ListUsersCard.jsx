6e70790e444004e47e2e1b9a66c83a082a12b2a2
Christine Uehara <42711372+cyuehara@users.noreply.github.com>
2021-07-29 02:25:48 -1000

    All Profile Page
    ListUserPage
    ListUserWidget (filtering)
    ListUserCard (name, bio, interests)
    
    need to work on pop up modal

---
+++ app/imports/ui/components/ListUsersCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Item, Grid, Divider, Header, Image, Label } from 'semantic-ui-react';
+
+class ListUsersCard extends React.Component {
+  render() {
+    /* const [open, setOpen] = React.useState(false); */
+    const aboutme = [
+      ' Hi, my name is Anne Jean Eyers and I am a senior in computer engineering at UH Manoa. I am interested in software ' +
+      'engineering, artificial intelligence, robotics, circuits, and machine learning. I am looking to find a faculty advisor ' +
+      'who can help me develop a project to fulfill my capstone project requirement for graduation a',
+    ].join(' ');
+    return (
+
+      <Item style={{ padding: '0rem 1.5rem 0.5rem 1.5rem' }}>
+        <Item.Content>
+          <Item.Header>
+            <Header as={'name'} inverted style={{ paddingTop: '1.5 rem' }}>
+              <Image size='huge' circular src="/images/annePic.png"/>
+              Anne Jean Eyers, Computer Engineer, Senior
+            </Header>
+          </Item.Header>
+          <Item.Description>
+            <Grid.Column>
+              <bio>
+                <Header sub as={'h4'} style={{ paddingTop: '0.5rem ' }} inverted>
+                  About Me
+                </Header>
+                <span>{aboutme}</span>
+              </bio>
+            </Grid.Column>
+            <Divider hidden>
+              <Item.Extra>
+                <Label size='tiny'>Machine Learning</Label>
+                <Label size='tiny'>Robotics</Label>
+              </Item.Extra>
+            </Divider>
+          </Item.Description>
+        </Item.Content>
+      </Item>
+    );
+  }
+}
+
+export default ListUsersCard;
---
+++ app/imports/ui/pages/ListUsers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ListUsersWidget from '../components/ListUsersWidget';
+
+class ListUsers extends React.Component {
+  render() {
+    return (
+      <ListUsersWidget />
+    );
+  }
+}
+
+export default (ListUsers);

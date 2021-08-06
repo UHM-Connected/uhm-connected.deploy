a5eff7f3f440734361bd6b211e5ae5e79c30af06
Joshua Paino <62793439+joshipaino@users.noreply.github.com>
2021-08-05 19:08:03 -1000

    created initial projectsCollection; need to fix form submission

---
+++ app/imports/api/project/Project.js
@@ -0,0 +1,39 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The ProjectsCollection. It encapsulates state and variable values for stuff.
+ */
+class ProjectsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ProjectsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      projectName: String,
+      faculty: String,
+      department: String,
+      goal: String,
+      studentRequirements: String,
+      description: String,
+      researchArea: Array,
+      'researchArea.$': String,
+      email: String,
+      owner: String,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the StuffsCollection.
+ * @type {StuffsCollection}
+ */
+export const Projects = new ProjectsCollection();
--- app/imports/ui/components/CreateProjectForm.jsx
+++ app/imports/ui/components/CreateProjectForm.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
-import { Button, Divider, Form, Grid, Segment, Header, Message, Select } from 'semantic-ui-react';
+import { Divider, Form, Grid, Segment, Header, Message, Select } from 'semantic-ui-react';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Projects } from '../../api/project/Project';
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  projectName: String,
+  faculty: String,
+  department: String,
+  goal: String,
+  studentRequirements: String,
+  description: String,
+  researchArea: Array,
+  'researchArea.$': String,
+  email: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
 
 const options = [
   { key: 'a', text: 'Algorithms', value: 'algorithms' },
@@ -7,25 +27,41 @@ const options = [
 ];
 
 class CreateNewProject extends React.Component {
+
+  submit(data, formRef) {
+    const { projectName, faculty, department, goal, studentRequirements, description, researchArea, email } = data;
+    const owner = Meteor.user().username;
+    Projects.collection.insert({ projectName, faculty, department, goal, studentRequirements, description, researchArea, email, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
   render() {
+    let fRef = null;
     return (
       <Grid container centered>
         <Grid.Column>
           <Divider hidden/>
           <Segment inverted className='projectForm' very padded>
             <Header as='h3' textAlign='center'>Create New Project</Header>
-            <Form success>
+            <Form success ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)}>
               <Form.Group unstackable widths={3}>
-                <Form.Input required label='Name of Project' placeholder='Name of Project' />
-                <Form.Input required label='Name of Faculty' placeholder='Name of Faculty' />
+                <Form.Input required label='Name of Project' placeholder='Name of Project' name='projectName' />
+                <Form.Input required label='Name of Faculty' placeholder='Name of Faculty' name='faculty' />
                 <Form.Input required label='Department' placeholder='Department' />
               </Form.Group>
               <Form.Group unstackable widths={2}>
-                <Form.TextArea required label='Goal for the Project' placeholder='What is the goals for the project...' />
-                <Form.TextArea required label='Student Requirements' placeholder='What is required from the student...' />
+                <Form.TextArea required label='Goal for the Project' placeholder='What is the goals for the project...' name='goals' />
+                <Form.TextArea required label='Student Requirements' placeholder='What is required from the student...' name='studentRequirements' />
               </Form.Group>
               <Grid.Row>
-                <Form.TextArea required label='Project Description' placeholder='Tell us more about the project...' />
+                <Form.TextArea required label='Project Description' placeholder='Tell us more about the project...' name='description' />
               </Grid.Row>
               <Form.Group unstackable widths={2}>
                 <Form.Field
@@ -34,8 +70,9 @@ class CreateNewProject extends React.Component {
                   label='Research Area'
                   options={options}
                   placeholder='Research Area'
+                  name='researchArea'
                 />
-                <Form.Input required label='Email' placeholder='Email Address' />
+                <Form.Input required label='Email' placeholder='Email Address' name='email' />
               </Form.Group>
               <Grid>
                 <Grid.Column textAlign="center">
@@ -44,7 +81,7 @@ class CreateNewProject extends React.Component {
                     header='Form Completed'
                     content="New Project Created"
                   />
-                  <Button type='submit'>Create</Button>
+                  <Form.Button Content='Submit'/>
                 </Grid.Column>
               </Grid>
             </Form>

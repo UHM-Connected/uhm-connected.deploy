b6fdb8192a39a68814ccfc6c0e75a0453ac1e94b
Aaron Nonaka <31535687+nonakaa@users.noreply.github.com>
2021-08-06 16:38:10 -1000

    update home and navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -9,23 +9,21 @@ import { Roles } from 'meteor/alanning:roles';
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
-    const menuStyle = { marginBottom: '10px',backgroundColor:"#1a3b29" };
+    const menuStyle = { marginBottom: '10px',backgroundColor:"#1a3b29", borderBottom: `2px solid #b48415` };
     return (
       <Menu style={menuStyle} attached="top" borderless inverted>
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/" key=''>Home</Menu.Item>
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/AboutUs" key='AboutUs'>About Us</Menu.Item>
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/user" key='user'>My Profile</Menu.Item>
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/listProjects" key='listProjects'>List Projects</Menu.Item>
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/listusers" key='listusers'>List Users</Menu.Item>
         
-        {this.props.currentUser ? (
-          [<Menu.Item as={NavLink} activeClassName="active" exact to="/" key=''>Home</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/AboutUs" key='AboutUs'>About Us</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/user" key='user'>My Profile</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/listProjects" key='listProjects'>List Projects</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/listusers" key='listusers'>List Users</Menu.Item>]
-        ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
           <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
         ) : ''}
         <Menu.Item position="right">
           {this.props.currentUser === '' ? (
-            <Dropdown id="login-dropdown" text="Login" pointing="top right" icon={'user'}>
+            <Dropdown id="login-dropdown" text="Sign In/Sign Up" pointing="top right" icon={'user'}>
               <Dropdown.Menu>
                 <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
                 <Dropdown.Item id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
@@ -34,6 +32,7 @@ class NavBar extends React.Component {
           ) : (
             <Dropdown id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
               <Dropdown.Menu>
+                <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="My Profile" as={NavLink} exact to="/user"/>
                 <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
               </Dropdown.Menu>
             </Dropdown>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,21 +4,46 @@ import { Grid, GridRow, Image } from 'semantic-ui-react';
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+
+    
+
     return (
       <div style={{fontFamily:'Poppins'}}>
         <grid>
-          <row>
-           <h1 style={{color: "white", fontSize:100, textAlign: 'center', fontFamily:'Poppins' }}>Welcome to UHM Connected</h1>
+          
+          <div class="loading">
+            <Image src="https://manoa.hawaii.edu/wp/wp-content/uploads/2018/04/manoa-lowercampus-hero-1024x576.jpg" centered size='fitted'
+            
+            ></Image>
+          <div class="textblock">
+           <h1 style={{color: "white",fontSize:50, fontFamily:'Poppins' }} >Welcome to UHM Connected</h1>
+           </div>
+          </div>
+          <row style={{display: 'flex', justifyContent: 'center'}}>
+            
+          
+            <a href="/#/listusers">
+             <button style={{backgroundColor: "#b48415", color: "white", fontSize:50, fontFamily:'Poppins'}}>Get Connected</button>
+            </a>
+          
           </row>
           <row style={{display: 'flex', justifyContent: 'center'}}>
-            <a href="/#/user">
-              <button style={{backgroundColor: "#b48415", color: "white", fontSize:50, fontFamily:'Poppins'}}>Get Connected</button>
-            </a>
+            <p style={{color: "white", textAlign: 'center', fontFamily:'Poppins', border:'3px solid #b48415', width: '50%', display:'center' }}>
+              Our goal is to help students connect with faculty to find opportunities to engage in research, hands-on experiences, and suitable internships, 
+              while helping faculty find students who are interested in participating in projects.</p>
+          
+
           </row>
         </grid> 
+
+         
       </div>
     );
   }
 }
+
+
+
+
 
 export default Landing;
